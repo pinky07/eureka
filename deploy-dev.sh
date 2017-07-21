e92227a4af864033b891375e2ac8d7cb3e8c04dc
Rubén Jiménez Goñi <ruben.jimenez@gft.com>
2017-07-21 11:49:58 -0600

    Changed ports

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -2,4 +2,4 @@
 #
 IMAGE_NAME="com.gft/eureka:0.0.1-SNAPSHOT"
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=$IMAGE_NAME --format="{{.ID}}"))
-docker run -e "SPRING_PROFILES_ACTIVE=dev" -d -p 11002:11002 $IMAGE_NAME
+docker run -e "SPRING_PROFILES_ACTIVE=dev" -d -p 11000:8080 $IMAGE_NAME
