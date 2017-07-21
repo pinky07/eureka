01aa5b95fd114a0cbcb9d675855ae23d8533ea0f
Rubén Jiménez Goñi <ruben.jimenez@gft.com>
2017-07-21 13:37:21 -0600

    Improved deploy script

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,5 +1,44 @@
 #!/bin/sh
 #
-IMAGE_NAME="com.gft/eureka:0.0.1-SNAPSHOT"
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=$IMAGE_NAME --format="{{.ID}}"))
+
+IMAGE_NAME='com.gft/eureka:0.0.1-SNAPSHOT'
+
+echo 'Creating new container based on' $IMAGE_NAME
+
+echo 'Searching for previously running containers...'
+CONTAINERS_RUNNING=`docker ps -a -q --filter ancestor=$IMAGE_NAME --format="{{.ID}}"`
+
+if test -n "$CONTAINERS_RUNNING";
+then
+
+	echo 'Previously running containers based on the same image are:'
+	echo $CONTAINERS_RUNNING
+
+	echo 'Trying to stop previously running containers...'
+	CONTAINERS_STOPPED=`docker stop $CONTAINERS_RUNNING`
+
+	if test -n "$CONTAINERS_STOPPED";
+	then
+		echo "Successfully stopped containers"
+
+		echo "Trying to remove previously running containers"
+		CONTAINERS_REMOVED=`docker rm $CONTAINERS_STOPPED`
+
+		if test -n "$CONTAINERS_STOPPED";
+		then
+			echo "Successfully removed containers"
+		else
+			echo "ERROR: Couldn't remove previously running containers!"
+		fi
+
+	else
+		echo "ERROR: Couldn't stop previously running containers!"
+	fi
+
+else
+	echo 'No previously running containers were found'
+fi
+
+echo 'Launching new container...'
 docker run -e "SPRING_PROFILES_ACTIVE=dev" -d -p 11000:11000 $IMAGE_NAME
+echo 'Success'
