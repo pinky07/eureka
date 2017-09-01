12742aba2b07b9cfc9ba956c1ddb13e8f9dd4e25
Rubén Jiménez Goñi <ruben.jimenez@gft.com>
2017-09-01 11:43:21 -0600

    Fixed QA Spring profiles active environment variable

--- jenkins/qa-2-deploy.sh
+++ jenkins/qa-2-deploy.sh
@@ -4,7 +4,7 @@
 IMAGE_NAME='com.gft.eureka.qa:latest'
 echo 'Launching new container based on image' $IMAGE_NAME '...'
 
-docker run -e 'SPRING_PROFILES_ACTIVE=default,dev' -d -p 11021:11021 $IMAGE_NAME
+docker run -e 'SPRING_PROFILES_ACTIVE=default,qa' -d -p 11021:11021 $IMAGE_NAME
 
 echo 'Waiting 20s for APP container to start...'
 sleep 20s
